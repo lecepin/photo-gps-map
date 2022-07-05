01be3ffd8ce525c653e7a510fef8259e884bec2e
lecepin <383810086@qq.com>
2022-07-05 12:38:25 +0800

    feat: init

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+mod utils;
+use wasm_bindgen::prelude::*;
+
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, photo-gps-map!");
+}
