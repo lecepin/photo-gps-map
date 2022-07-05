112ffede447d4686d8ec46a25b122adbd71bd24b
lecepin <383810086@qq.com>
2022-07-05 13:16:18 +0800

    feat: log

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,18 @@
 mod utils;
 use wasm_bindgen::prelude::*;
+use web_sys::{console::log_1 as log, window};
 
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+pub fn greet() {
+    log(&JsValue::from(format!("{} {}", "Greet", "World")));
+    window().unwrap().alert_with_message("Greet from Rust!");
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, photo-gps-map!");
+#[wasm_bindgen(start)]
+pub fn run() {
+    utils::set_panic_hook();
 }
